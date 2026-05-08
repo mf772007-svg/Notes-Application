8c413d812745c5b7b05f54820f4ca71b81632f13
Mohamed Farag <mf772007@gmail.com>
2026-05-08 23:08:20 +0300

    create notes view and change them

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Notes App')),
+      body: const Center(child: Text('Welcome to the Notes App!')),
+    );
+  }
+}
